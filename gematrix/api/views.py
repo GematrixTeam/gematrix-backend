8a2c421e2802636701ff1d6563d7a224f63ead4d
reberu <redlightexit@gmail.com>
2019-10-14 21:17:25 +0900

    Исправления по коду. Подключен фреймворк djangorestframework.

--- gematrix/api/views.py
+++ gematrix/api/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
 
 
-def test_view(request):
-    return HttpResponse(status=200)
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
